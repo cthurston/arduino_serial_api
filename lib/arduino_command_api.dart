88d61cc34a3c3f8fa8743361e0086cc10470f621
Christopher Thurston <chris.thurston@gmail.com>
2020-12-14 21:42:24 -0700

    Adding blocking readline.

--- lib/arduino_command_api.dart
+++ lib/arduino_command_api.dart
@@ -1,15 +1,18 @@
+import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:dart_serial_port/dart_serial_port.dart';
 
-const libraryVersion = 'V0.7';
+const libraryVersion = 'V0.8';
 const encoder = Utf8Encoder();
 const decoder = Utf8Decoder();
 const int DIGITAL_HIGH = 1;
 const int DIGITAL_LOW = 0;
 const int PIN_INPUT = 0;
 const int PIN_OUTPUT = 1;
+const serialTimeoutMs = 1500;
 
 /// Build a command string that can be sent to the arduino.
 /// Input:
@@ -20,59 +23,135 @@ Uint8List buildCmdString(String cmd, [List<dynamic> args = const []]) {
   return encoder.convert('@$cmd%$a\$!');
 }
 
-String getSketchVersion(SerialPort sp) {
-  var cmd = buildCmdString('version');
-  sp.write(cmd);
-  sp.flush();
-  return readAvailableLine(sp);
-}
-
 String readAvailableLine(SerialPort sp) {
-  var bytes = 8;
-  var buf = sp.read(bytes);
+  // while (sp.bytesAvailable == 0) {
+  //   sleep(Duration(milliseconds: 1));
+  // }
+  // var bytes = sp.bytesAvailable;
+  print(sp.bytesAvailable);
+  var buf = sp.read(sp.bytesAvailable, timeout: serialTimeoutMs);
   var line = decoder.convert(buf);
   return line.replaceAll('\r\n', '');
 }
 
-// void findPort(int timeout) {
-//   var i = 0;
-//   for (final name in SerialPort.availablePorts) {
-//     final sp = SerialPort(name);
-//     sp.openReadWrite();
-//     var v = sp.read(5, timeout: timeout);
+void listAvailablePorts() {
+  var i = 0;
+  for (var name in SerialPort.availablePorts) {
+    try {
+      var sp = SerialPort(name);
+      print('${++i}) $name');
+      print('\tDescription: ${sp.description}');
+      print('\tManufacturer: ${sp.manufacturer}');
+      print('\tSerial Number: ${sp.serialNumber}');
+      sp.dispose();
+    } catch (e) {}
+  }
+}
 
-//     print('${++i}) $name');
-//     print('\tDescription: ${sp.description}');
-//     print('\tManufacturer: ${sp.manufacturer}');
-//     print('\tSerial Number: ${sp.serialNumber}');
-//     // print('\tProduct ID: 0x${sp.productId.toRadixString(16)}');
-//     // print('\tVendor ID: 0x${sp.vendorId.toRadixString(16)}');
-//     sp.dispose();
-//   }
-// }
+String findPort() {
+  var port = '';
+  var arduinoMatcher = RegExp(r'arduino', caseSensitive: false);
+  for (var name in SerialPort.availablePorts) {
+    try {
+      var sp = SerialPort(name);
+      if (sp.manufacturer != null && sp.manufacturer.contains(arduinoMatcher)) {
+        port = name;
+        sp.dispose();
+        break;
+      }
+    } catch (e) {}
+  }
+
+  return port;
+}
 
 class Arduino {
   int timeout = 2000;
+  int baudRate;
   late SerialPort sp;
   late SPI spi;
+  // late SerialPortReader reader;
+  String message = '';
+  Completer _serialResponse = Completer();
 
   Arduino({
-    int baudRate = 115200,
+    this.baudRate = 115200,
     String? port,
     SerialPort? serialPort,
   }) {
+    if (port == null && serialPort == null) {
+      port = findPort();
+    }
+
     sp = serialPort ?? SerialPort(port);
+
+    // reader = SerialPortReader(sp, timeout: serialTimeoutMs);
+    // reader.stream.map((d) => decoder.convert(d)).listen((data) {
+    //   print('some data $data');
+
+    //   message += data;
+
+    //   if (message.contains('\r\n')) {
+    //     var parts = message.split('\r\n');
+    //     _serialResponse.complete(parts.first);
+
+    //     message = parts.length > 1 ? parts[1] : '';
+    //   }
+    // });
+
+    spi = SPI(this);
+  }
+
+  Future<dynamic> start() async {
+    var open = sp.openReadWrite();
+    if (!open) {
+      throw Exception(
+          'Could not connect to the Arduino. ${SerialPort.lastError}');
+    }
 
     var config = SerialPortConfig();
     config.baudRate = baudRate;
+    config.bits = 8;
+    config.stopBits = 1;
+    config.parity = SerialPortParity.none;
+    config.flowControl = SerialPortFlowControl.none;
+
     sp.config = config;
 
-    var success = sp.openReadWrite();
+    config.dispose();
 
-    print(success);
-    print(SerialPort.lastError);
+    sp.read(11, timeout: serialTimeoutMs); // should be 'connected'
+    sp.flush();
 
-    spi = SPI(this);
+    var version = getSketchVersion();
+    print('ver: $version');
+    if (libraryVersion != version) {
+      throw Exception(
+          'The Arduino sketch is not the correct version, ${libraryVersion}.');
+    }
+  }
+
+  String getSketchVersion() {
+    var cmd = buildCmdString('version');
+    sp.flush();
+    sp.write(cmd, timeout: serialTimeoutMs);
+    sp.drain();
+
+    var version = readline();
+    return version;
+  }
+
+  String readline() {
+    var timeout = Duration(milliseconds: 300);
+    var completed = false;
+    var line = '';
+    while (!completed && DateTime.now().isBefore(DateTime.now().add(timeout))) {
+      var buf = sp.read(sp.bytesAvailable, timeout: serialTimeoutMs);
+      line = decoder.convert(buf);
+      if (line.contains('\r\n')) completed = true;
+    }
+
+    return line.replaceAll('\r\n', '');
   }
 
   /// Sets I/O mode of pin
@@ -82,8 +161,8 @@ class Arduino {
       pin = -pin;
     }
     var cmd = buildCmdString('pm', [pin, mode]);
-    sp.write(cmd);
     sp.flush();
+    sp.write(cmd);
   }
 
   /// Send digitalWrite command to a pin
@@ -92,35 +171,35 @@ class Arduino {
       pin = -pin;
     }
     var cmd = buildCmdString('dw', [pin]);
-    sp.write(cmd);
     sp.flush();
+    sp.write(cmd);
   }
 
   /// return 1 for HIGH, 0 for LOW
   int digitalRead(int pin) {
     var cmd = buildCmdString('dr', [pin]);
-    sp.write(cmd);
     sp.flush();
-    return _readLastInt();
+    sp.write(cmd, timeout: serialTimeoutMs);
+    return int.parse(readline());
   }
 
   void analogWrite(int pin, int value) {
     var cmd = buildCmdString('aw', [pin, value]);
-    sp.write(cmd);
     sp.flush();
+    sp.write(cmd, timeout: serialTimeoutMs);
   }
 
   int analogRead(int pin) {
     var cmd = buildCmdString('ar', [pin]);
-    sp.write(cmd);
     sp.flush();
+    sp.write(cmd, timeout: serialTimeoutMs);
     return _readLastInt();
   }
 
   int _readLastInt() {
     // the analog value can only be up to 4095?
-    var bytes = 6;
-    Uint8List buf = sp.read(bytes);
+    var bytes = sp.bytesAvailable;
+    Uint8List buf = sp.read(bytes, timeout: serialTimeoutMs);
     var line = decoder.convert(buf);
     var ans = int.parse(line.replaceAll('\r\n', ''));
     return ans;
--- test/arduino_command_api_test.dart
+++ test/arduino_command_api_test.dart
@@ -1,6 +1,5 @@
 // @dart=2.9
 import 'package:arduino_command_api/arduino_command_api.dart';
-import 'package:dart_serial_port/dart_serial_port.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -10,17 +9,13 @@ void main() {
         buildCmdString('version', [4, 5.1, 'test']), '@version%4%5.1%test\$!');
   });
 
-  test('arduino', () {
-    var port = '/dev/cu.Bluetooth-Incoming-Port';
-    var a = Arduino(port: port);
+  test('arduino', () async {
+    var port = '/dev/cu.usbmodem141401';
+    var a = Arduino();
+    await a.start();
   });
 
-  test('junk', () {
-    print('hey\r\n');
-    print('heya\r\n'.replaceAll('\r\n', ''));
-
-    print(int.parse('405') + 45);
-    var spc = SerialPortConfig();
-    spc.baudRate = 112500;
+  test('find port', () {
+    findPort();
   });
 }
